204e2139cb6840af28fb4c0fb1d7410e4981d3f8
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-03-16 17:49:46 -0500

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var table = require ("cli-table");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: "",
+    port: "3306",
     user: "root",
     password:"",
     database: "bamazon"
